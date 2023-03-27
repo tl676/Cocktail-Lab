9802c5ff01f738aa781913fae2389e07bec4d94e
Tina Li <tl676@cornell.edu>
2023-03-27 05:04:23 -0400

    returning responses for flavor pref only

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
+from cocktailLab import CocktailLab
 
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
@@ -12,9 +13,9 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 MYSQL_USER = "root"
-MYSQL_USER_PASSWORD = "MayankRao16Cornell.edu"
+MYSQL_USER_PASSWORD = ""
 MYSQL_PORT = 3306
-MYSQL_DATABASE = "kardashiandb"
+MYSQL_DATABASE = "cocktaildb"
 
 mysql_engine = MySQLDatabaseHandler(MYSQL_USER,MYSQL_USER_PASSWORD,MYSQL_PORT,MYSQL_DATABASE)
 
@@ -24,22 +25,26 @@ mysql_engine.load_file_into_db()
 app = Flask(__name__)
 CORS(app)
 
+cocktailLab = CocktailLab()
+
 # Sample search, the LIKE operator in this case is hard-coded, 
 # but if you decide to use SQLAlchemy ORM framework, 
 # there's a much better and cleaner way to do this
-def sql_search(episode):
-    query_sql = f"""SELECT * FROM episodes WHERE LOWER( title ) LIKE '%%{episode.lower()}%%' limit 10"""
-    keys = ["id","title","descr"]
-    data = mysql_engine.query_selector(query_sql)
-    return json.dumps([dict(zip(keys,i)) for i in data])
+# def sql_search(episode):
+#     query_sql = f"""SELECT * FROM episodes WHERE LOWER( title ) LIKE '%%{episode.lower()}%%' limit 10"""
+#     keys = ["id","title","descr"]
+#     data = mysql_engine.query_selector(query_sql)
+#     return json.dumps([dict(zip(keys,i)) for i in data])
 
 @app.route("/")
 def home():
-    return render_template('base.html',title="sample html")
+    return render_template('base.html', title="sample html")
 
-@app.route("/episodes")
+@app.route("/query")
 def episodes_search():
-    text = request.args.get("title")
-    return sql_search(text)
+    text = request.args.get("flavor_prefs")
+    # return sql_search(text)
+    print(json.dumps(cocktailLab.cos_query(text)))
+    return json.dumps(cocktailLab.cos_query(text))
 
 # app.run(debug=True)
---
+++ backend/cocktailLab.py
@@ -0,0 +1,181 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import csv
+
+
+class CocktailLab:
+    def __init__(self):
+        # Initialize tfidf
+        self.tags_data = self.read_file('data/cocktail_tags.csv')
+
+        self.num_cocktails = len(self.tags_data)
+
+        self.cocktail_name_to_index = {
+            name: index for index, name in enumerate(self.tags_data.keys())
+        }
+
+        self.cocktail_index_to_name = {
+            v: k for k, v in self.cocktail_name_to_index.items()}
+
+        self.cocktail_names = self.tags_data.keys()
+
+        self.tags_tfidf_vectorizer = self.make_vectorizer()
+
+        tags = [self.tags_data[cocktail] for cocktail in self.tags_data]
+        self.tags_tfidf_matrix = self.tags_tfidf_vectorizer.fit_transform(
+            tags).toarray()
+        self.index_to_vocab = {i: v for i, v in enumerate(
+            self.tags_tfidf_vectorizer.get_feature_names())}
+
+    def read_file(self, file):
+        """ Returns a dictionary of format {'cocktail name' : 'tag1,tag2'}
+        Parameters:
+        file: name of file
+
+        Note: currently configured for cocktail_tags.csv (tags only)
+        """
+        with open(file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            out = {}
+            for row in csv_reader:
+                if line_count == 0:
+                    # print(f'Column names are {", ".join(row)}')
+                    line_count += 1
+                else:
+                    out[row[1].lower()] = row[2].lower()
+        return out
+
+    def make_vectorizer(self, binary=False, max_df=1.0, min_df=1, use_stop_words=True):
+        """ Returns a TfidfVectorizer object with the above preprocessing properties.
+
+        By default this function returns a tf-idf matrix representation of data. 
+        This can be switched to a binary representation by setting the binary param 
+        to True.
+
+        Parameters:
+        binary: bool (Default = False)
+            A flag to switch between tf-idf representation and binary representation
+        max_df: float (Defualt = 1.0)
+            The maximum document frequency to use for the matrix, as a proportion of 
+            docs.
+        min_df: float or int (Default = 1)
+            The miniumum document frequency to use for the matrix. If [0.0,1.0], 
+            the parameter represents a proportion of documents, otherwise in absolute
+            doc counts. 
+        use_stop_words: bool (Default = True)
+            A flag to let sklearn remove common stop words.
+
+        Returns:
+        A #doc x #vocab np array
+
+        """
+        if binary:
+            use_idf = False
+            norm = None
+        else:
+            use_idf = True
+            norm = 'l2'
+
+        if use_stop_words:
+            stop_words = 'english'
+        else:
+            stop_words = None
+
+        tf_mat = TfidfVectorizer(max_df=max_df, min_df=min_df,
+                                 stop_words=stop_words, use_idf=use_idf,
+                                 binary=binary, norm=norm)
+
+        return tf_mat
+
+    def make_query(self, tokens, vectorizer, doc_by_vocab):
+        """ Returns a query vector made from tokens that matches the term matrix 
+            doc_by_vocab.
+
+        Parameters:
+        tokens: str list or str set
+            The tokens that make up a query. 
+        vectorizer: tfidf vectorizer object
+            The doc by vocab matrix as computed by make_matrix
+        doc_by_vocab: tfidf or boolean matrix
+        """
+        vocab_to_index = {v: i for i, v in enumerate(
+            vectorizer.get_feature_names())}
+        retval = np.zeros_like(doc_by_vocab[0])
+        for t in tokens:
+            try:
+                ind = vocab_to_index[t]
+                retval[ind] = 1
+            except:
+                # token not in matrix
+                continue
+        return retval
+
+    def cos_sim(self, vec1, vec2):
+        """ Returns the cos sim of two vectors.
+
+        Helper for cos_rank
+        """
+        num = np.dot(vec1, vec2)
+        den = (np.linalg.norm(vec1)) * (np.linalg.norm(vec2))
+        return num / den
+
+    def cos_rank(self, query, doc_by_vocab):
+        """ Returns a tuple list that represents the doc indexes and their
+            similarity scores to the query.
+
+            Known Problems: This needs to be updated to use document ids (when we 
+                            make those).
+
+            Params:
+            query: ?? by 1 string np array
+                The desired ingredients, as computed by make_query
+            doc_by_vocab: ?? by ?? np array
+                The doc by vocab matrix as computed by make_matrix
+
+            Returns:
+                An (int, int) list where list[0] is the doc index, and list[1] is
+                the similarity to the query.
+        """
+        retval = []
+
+        for d in range(len(doc_by_vocab)):
+            doc = doc_by_vocab[d]
+            sim = self.cos_sim(query, doc)
+            retval.append([d, sim])
+        return list(sorted(retval, reverse=True, key=lambda x: x[1]))
+
+    def cos_query(self, query):
+        q = self.make_query([word.strip().lower() for word in query.split(",")], 
+                            self.tags_tfidf_vectorizer,
+                            self.tags_tfidf_matrix)
+        rank_list = self.cos_rank(q, self.tags_tfidf_matrix)
+        drink_name_list = [self.cocktail_index_to_name[i[0]] for i in rank_list]
+        # print(drink_name_list)
+        return drink_name_list
+
+    def boolean_search(self, query, doc_by_vocab):
+        """ Returns a list of doc indexes that contain all the query words
+
+        Params:
+        query: ?? by 1 string np array
+            The desired ingredients, as computed by make_query
+        doc_by_vocab: ?? by ?? np array
+            The doc by vocab matrix as computed by make_matrix
+
+        Returns:
+            An int list where each element is the doc index of a doc containing 
+            all the query words.
+        """
+        retval = []
+        target = np.sum(query)
+        for d in range(len(doc_by_vocab)):
+            doc = doc_by_vocab[d]
+            combine = np.logical_and(query, doc)
+            if np.sum(combine) == target:
+                retval.append(d)
+        return retval
+
+
+if __name__ == "__main__":
+    cocktail = CocktailLab()
